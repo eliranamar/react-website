cba12fa5813b402427a408f8f84f858c264c13a1
eliranamar <eliranamar7@gmail.com>
2017-07-29 18:03:25 +0300

    have all pages

---
+++ client/src/WeatherApp/common/Deals.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Deals extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="row text-center">
+          <h2>Deals</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea velit, alias culpa id ad quia cumque sed aperiam eos rerum necessitatibus neque quibusdam et veniam! Consequuntur aperiam alias amet et nostrum tenetur doloremque adipisci aliquid eligendi atque natus ratione commodi aliquam labore sint reprehenderit, rem nihil modi magnam ducimus accusantium.</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Deals;
