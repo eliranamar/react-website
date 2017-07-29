3e165c98fd23e7a5543d6214cf0e0c61fe5c4f02
iradkot <irad16@gmail.com>
2017-07-29 16:30:20 +0300

    navbar-basic

--- client/dist/js/app.js
+++ client/dist/js/app.js
@@ -23461,6 +23461,8 @@ var _react = __webpack_require__(6);
 
 var _react2 = _interopRequireDefault(_react);
 
+var _reactRouterDom = __webpack_require__(92);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -23482,9 +23484,44 @@ var App = function (_React$Component) {
     key: 'render',
     value: function render() {
       return _react2.default.createElement(
-        'div',
-        null,
-        'Header'
+        'ul',
+        { className: 'nav nav-tabs' },
+        _react2.default.createElement(
+          'li',
+          { role: 'presentation' },
+          _react2.default.createElement(
+            _reactRouterDom.Link,
+            { to: '/' },
+            'Home'
+          )
+        ),
+        _react2.default.createElement(
+          'li',
+          { role: 'presentation' },
+          _react2.default.createElement(
+            _reactRouterDom.Link,
+            { to: '/about' },
+            'About'
+          )
+        ),
+        _react2.default.createElement(
+          'li',
+          { role: 'presentation' },
+          _react2.default.createElement(
+            _reactRouterDom.Link,
+            { to: '/deals' },
+            'Deals'
+          )
+        ),
+        _react2.default.createElement(
+          'li',
+          { role: 'presentation' },
+          _react2.default.createElement(
+            _reactRouterDom.Link,
+            { to: '/contact' },
+            'Contact'
+          )
+        )
       );
     }
   }]);
--- client/src/WeatherApp/common/Header.js
+++ client/src/WeatherApp/common/Header.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 class App extends React.Component {
   render() {
     return (
-      <div>
-        Header
-      </div>
+        <ul className="nav nav-tabs">
+          <li role="presentation"><Link to="/">Home</Link></li>
+          <li role="presentation"><Link to="/about">About</Link></li>
+          <li role="presentation"><Link to="/deals">Deals</Link></li>
+          <li role="presentation"><Link to="/contact">Contact</Link></li>
+        </ul>
     );
   }
 }
